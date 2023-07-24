709679dbbd66f1fe93ffa7ff713bfc11bf5c615a
JuiceWRLD <999alishafiei999@gmail.com>
2023-07-24 02:26:18 +0200

    :)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { Box, CssBaseline } from "@mui/material";
+import { Box, Button, CssBaseline } from "@mui/material";
 import Header from "./components/Header";
 import { useState } from "react";
 import Sidebar from "./components/Sidebar";
 import Main from "./components/Main";
+
+import { getDatabase, ref, child, get } from "firebase/database";
+import { app } from "./firebaseConfig";
 
 // Create your light and dark themes
 const lightTheme = createTheme({
@@ -19,7 +22,22 @@ const darkTheme = createTheme({
 });
 
 const App = () => {
-  const [theme, setTheme] = useState(darkTheme); // Initial theme is light
+  const [theme, setTheme] = useState(darkTheme); // Initial theme
+  const dbRef = ref(getDatabase(app));
+
+  const click = () => {
+    get(child(dbRef, `todos`))
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          console.log(snapshot.val());
+        } else {
+          console.log("No data available");
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
   const toggleTheme = () => {
     setTheme((prevTheme) =>
@@ -34,6 +52,7 @@ const App = () => {
         <Header onClick={toggleTheme} />
         <Sidebar />
         <Main />
+        <Button onClick={click}>Ali</Button>
       </Box>
     </ThemeProvider>
   );
